b3861e8eede7686a9019544f9dc1ab9ec1cec3fd
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2022-12-13 12:55:26 +0100

    flake8

--- src/derivationtree/dtree.py
+++ src/derivationtree/dtree.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with ISLa.  If not, see <http://www.gnu.org/licenses/>.
 
-import copy
 import html
 import json
 import re
